7e0fc33e40db2adcc5a96a81a6b7fc1cd4e10993
Vasyl Shumeiko <vasyl.shumeiko@gmail.com>
2019-05-12 00:37:33 +0300

    redux with comments

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,16 +5,18 @@ import {increment} from '../AC';
 
 class Counter extends Component {
   static propTypes = {
-    counter: PropTypes.number
+    counter: PropTypes.number,
+    increment: PropTypes.func.isRequired
   };
 
   handleIncrement = () => {
-    console.log("---", "Incrementing");
     const {increment} = this.props;
-    increment()
+    increment();
+    // this.props.dispatchIncrement()
   };
 
   render() {
+    console.log(this.props);
     return (
       <div>
         <h2>{this.props.counter}</h2>
@@ -27,3 +29,26 @@ class Counter extends Component {
 export default connect((state) => ({
   counter: state.count
 }), {increment})(Counter);
+
+
+
+// // -----Different way------
+//
+// // Дістає дані з `store` і передає нашому компонентові
+// function mapStateToProps(state) {
+//   return {
+//     counter: state.count
+//   }
+// }
+//
+// const mapToDispatch = {
+//   dispatchIncrement: increment
+// };
+//
+// // Метод `connect` для з'єднання компоненти з `store`.
+// // Другий параметр загортає `Action Creator` в `dispatch`.
+// // Передаємо другим параметром об'єкт `mapToDispatch`
+// // Замість того, щоб не писати в функції `handleIncrement` таке: `this.props.dispatch(increment())` напишемо `this.props.dispatchIncrement()`
+// const decorator = connect(mapStateToProps, mapToDispatch);
+//
+// export default decorator(Counter);
